3ecf158df52a8529a716f9e174a01036297e0749
Nikhil Metrani <a0135935@u.nus.edu>
2016-05-14 15:42:07 +0800

    Displaying readout from analogur IO board on 4x20 LCD display

---
+++ source/03_analogueio/04_boardtolcd.py
@@ -0,0 +1,25 @@
+# Copyright 2016 Nikhil Metrani
+# Institute of Systems Science, National University of Singapore
+#
+# Displaying readout from analogur IO board on 4x20 LCD display
+# Connections:
+#    VCC:        2
+#    Ground:     6
+#    SDA:        3 (GPIO 2)
+#    SCL:        5 (GPIO 3)
+#
+
+from IOBoard import *
+from Lcd import *
+
+
+def main():
+    board = Board()
+    lcd = Lcd()
+    while (1):
+        lcd.clear()
+        lcd.display_string("Control: %d" % (board.control()), 1)
+        lcd.display_string("Light: %d" % (board.light()), 1)
+        lcd.display_string("Temprature: %d" % (board.temperature()), 1)
+        lcd.display_string("Custom: %d" % (board.custom()), 1)
+        time.sleep(1)
